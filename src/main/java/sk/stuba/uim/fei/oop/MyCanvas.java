2f407baabfa0bf2e0a0646eb6536bb782c155343
Theodora2000 <xsimont@stuba.sk>
2021-03-23 11:09:01 +0100

    graphic in for

--- src/main/java/sk/stuba/uim/fei/oop/MyCanvas.java
+++ src/main/java/sk/stuba/uim/fei/oop/MyCanvas.java
@@ -9,5 +9,12 @@ public class MyCanvas extends Canvas {
         g.fillRect(100,150,100,100);
         g.setColor(Color.red);
         g.fillPolygon(new int[]{100,200,150},new int[]{150,150,75},3);
+
+        g.setColor(Color.white);
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                g.fillRect(110+45*i,160+45*j,35,35);
+            }
+        }
     }
 }
