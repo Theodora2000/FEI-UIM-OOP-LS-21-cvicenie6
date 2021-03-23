605c9073b97168e8fb421a04a8061678debefea4
Theodora2000 <xsimont@stuba.sk>
2021-03-23 11:37:50 +0100

    action listener

--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -2,13 +2,28 @@ package sk.stuba.uim.fei.oop;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main implements ActionListener {
+
     public static void main(String []args){
+        new Main();
+
+    }
+    public Main(){
         var okno = new JFrame();
         okno.setVisible(true);
         okno.setSize(300,300);
 
-        okno.add(new MyCanvas());
+        var button = new JButton("Click me!");
+        okno.add(button);
+
+        button.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("hello");
     }
 }
